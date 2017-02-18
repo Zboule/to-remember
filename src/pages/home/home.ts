2fe898f311e02550accddb34b2a34c09c00816bf
Jordane CURE <jordane@MacBook-Pro-de-Jordane.local>
2017-02-18 13:56:37 -0500

    Ajout de

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  }
 
+  onStartQuizz (){
+    console.log("jordane");
   }
 
 }
